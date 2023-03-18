6456cfd3af37239fec5423d95b98737efabfbb8e
Hi-priest <onome.perez@gmail.com>
2023-03-18 16:37:03 -0700

    lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - pogram prints alphabets in lower cases
+ *
+ * Return: Answer 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while
+		(c <= 'z');
+	{
+		putchar(c);
+	}
+	return (0);
+}
