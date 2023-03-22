b7ba6ec1fc636253b1012f24ab1be194424b597e
Hi-priest <onome.perez@gmail.com>
2023-03-22 14:43:46 -0700

    _abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value
+ * @int: input type
+ * Return: Always 0 (success)
+ */
+
+int _abs(int j)
+{
+	if (j >= 0)
+	{
+		return (j);
+	}
+	return (-j);
+}
