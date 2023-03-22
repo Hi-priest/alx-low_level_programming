4d94fe48929bbd737ae4eaa1e5462e0f2f8b6370
Hi-priest <onome.perez@gmail.com>
2023-03-21 17:05:31 -0700

    lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - prints alphabets in lowercase
+ *
+ * return: 0
+ */
+
+void print_alphabet(void);
+{
+	char letter;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
