ae7c256ee74638daa0ef0c4990ea22fd5acf2e6a
Hi-priest <onome.perez@gmail.com>
2023-03-28 04:25:50 -0700

    length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * int _strlen - fuction returns the length of a string
+ * @s: string length
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+
+	return (count);
+}
